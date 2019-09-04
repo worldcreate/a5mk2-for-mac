c2ab7807c05441bd5b0646556ab5e6a5443b3585
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2019-09-04 17:44:37 +0900

    Initial Commit

---
+++ a5mk2_mirror/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  a5mk2_mirror
+//
+//  Created by matsumoto on 2019/09/04.
+//  Copyright © 2019 zyyx. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ a5mk2_mirror/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  a5mk2_mirror
+//
+//  Created by matsumoto on 2019/09/04.
+//  Copyright © 2019 zyyx. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
